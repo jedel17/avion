70dc955103d3c8b82c920b256067f64af9fc1136
jdel17 <jessica.delourme@sfr.fr>
2023-01-01 10:29:16 +0100

    controle embarquement

--- index.py
+++ index.py
@@ -15,40 +15,44 @@ def page_index ():
 def page_cdbord():
     poidtotal = 0
     for passager in passagers:  # on parcours la liste des passagers
-        poidtotal = poidtotal + int(passager['poid'])# a partir du poids du bagage du passager je determine le poids total de bagage
+        poidtotal = poidtotal + int(passager['poid']) # a partir du poids du bagage du passager je determine le poids total de bagage
     return render_template('cdbord.html',passagers=passagers,poid=poidtotal)
 
 @app.route('/embarquement', methods=['GET', 'POST'])
 def page_embarquement():
-    if request.method == 'POST':
-        data2 = request.form.to_dict()
-        passeport_identifiant= "non"
+    if request.method == 'POST':  # si on recoit des données de formulaires html
+        formulaire = request.form.to_dict()  # decode les donnees du formulaire html dans un dictionnaire
+        passeport_found = False  # pour l'instant on a pas trouvé le passeport
         for passager in passagers:  # on parcours la liste des passagers
-            if passager['passeport'] == formulaire['passeportid']:  # si on trouve le passeport
-                embarque =="oui"  # le passager a embarqué
-                passeport_identifiant = "oui"  # on a trouvé le passeport
+            if passager['passeportid'] == formulaire['passeportid']:  # si on trouve le passeport
+                passager['embarque'] = True  # le passager a embarqué
+                passeport_found = True  # on a trouvé le passeport
 
-
-        if passeport_identifiant=="oui":  # si on atrouvé le passeport
+        if passeport_found == True:  # si on atrouvé le passeport
             return redirect(url_for('page_index'))  # on redirige vers la page d'acceuil
         else:
             return redirect(url_for('page_errpassport'))  # sinon on redirige vers l'erreur passeport
 
-        return render_template('cdbord.html')
+    return render_template('embarquement.html')
 
 @app.route('/bagages', methods=['GET', 'POST'])
 def page_bagages():
-    if request.method == 'POST':  # si on recoit des données de formulaires html
         global passagers
-        data = request.form.to_dict()  # decode les donnees du formulaire html dans un dictionnaire
-        if 'button_save' in data:
-            passagers.append(data)
+        if request.method == 'POST':  # si on recoit des données de formulaires html
+            formulaire = request.form.to_dict()  # decode les donnees du formulaire html dans un dictionnaire
+            # construit le dictionnaire passager
+            passager = {'passeportid': formulaire['passeportid'], "name": formulaire['name'],
+                        "firstname": formulaire['firstname'],
+                        "poid": formulaire['poid'],"embarque":False}
+            passagers.append(passager)
             print(passagers)
+            print(formulaire['passeportid'])
         # redirige vers la page commandant de bord
-        return redirect(url_for('page_cdbord'))
-    return render_template('bagages.html')
+            return redirect(url_for('page_cdbord'))
+        return render_template('bagages.html')
 
 
 @app.route('/passeportintrouvable', methods=['GET','POST'])
 def page_passeportintrouvable():
+
     return render_template('passeportintrouvable.html')
